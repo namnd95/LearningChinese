1c9c4f04864685f0d6e21787080b8c326e756bd5
Nguyen Duc Nam <nguyendn@seagroup.com>
2018-06-17 18:30:40 +0700

    Add week 1 of hsk1

--- convert.py
+++ convert.py
@@ -4,6 +4,8 @@
 import codecs
 import sys
 
+from sets import Set
+
 def isSame(line):
     if len(line) <= 1:
         return False
@@ -36,9 +38,34 @@ def convert(input, output):
                     fo.write('%s\n' % curLine)
                     curLine = ''
             fo.write('%s\n' % curLine)
+
+def checkDup(input):
+    output = ''
+    last = ''
+    with codecs.open(input, 'r', encoding='utf8') as fi:
+        s = Set()
+        for line in fi:
+            line = line.strip()
+            output = output + line + '\n'
+            for c in line:
+                if c not in s:
+                    last = last + c
+                    s.add(c)
+
+    with codecs.open(input, 'w', encoding='utf8') as fo:
+        fo.write('%s' % output)
+        fo.write('%s' % last)
+
+    
+        
+
+                   
             
             
 # python convert.py wordlist/hsk2-words.txt wordlist/hsk2-list.txt
 if __name__ == "__main__":
     # convert(wordlist/hsk2-words.txt, wordlist/hsk2-list.txt)
-    convert(sys.argv[1], sys.argv[2])
+    if sys.argv[1] == 'convert':
+        convert(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'check':
+        checkDup(sys.argv[2])
